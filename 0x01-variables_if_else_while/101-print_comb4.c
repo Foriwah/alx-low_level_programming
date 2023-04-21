e0ff65e2d5e5b7b52561d41fbf47dea815643d74
Foriwah <akosuaforiwah00@gmail.com>
2023-04-21 09:59:51 -0700

    Three digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Generate a combination
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	int n;
+	int a;
+	int b;
+
+	for (n = 0; n < 8; n++)
+	{
+		for (a = (n + 1); a < 9; a++)
+		{
+			for (b = (n + 2); b < 10; b++)
+			{
+				if (n == a && n == b)
+					continue;
+				else
+				{
+					putchar(48 + n);
+					putchar(48 + a);
+					putchar(48 + b);
+					if (n == 7 && a == 8 && b == 9)
+						continue;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
