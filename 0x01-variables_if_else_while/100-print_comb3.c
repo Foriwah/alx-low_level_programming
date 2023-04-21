96800ac04b98aa9e6d43ad8a89d5e74fac45f6b3
Foriwah <akosuaforiwah00@gmail.com>
2023-04-21 09:09:15 -0700

    Combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print 2 digit numbers
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (a = (n +1); a < 10; a++)
+		{
+			if (n == a)
+				continue;
+			else
+			{
+				putchar(48 + n);
+				putchar(48 + a);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
