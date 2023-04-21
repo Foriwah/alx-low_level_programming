a0fa1b3509572cfd722966e1877bf3e5c4dd6892
Foriwah <akosuaforiwah00@gmail.com>
2023-04-21 14:54:40 -0700

    Wierd combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Combination of numbers
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = a; c < 10; c++)
+			{
+				for (d = (b + 1); d < 10; d++)
+				{
+					putchar(48 + a);
+					putchar(48 + b);
+					putchar(' ');
+					putchar(48 + c);
+					putchar(48 + d);
+					if (c == 9 && d == 9 && a == 9 && b == 8)
+						continue;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
