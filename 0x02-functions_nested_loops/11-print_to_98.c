587455a8d05260b9ee5cdf8bc9c77d875d3ebf8e
Foriwah <akosuaforiwah00@gmail.com>
2023-04-25 10:15:16 -0700

    Natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,36 +8,17 @@
 
 void print_to_98(int n)
 {
-	int a;
-	int b;
 
 	if (n > 98)
 	{
-		for (a = n; a >= 98; a--)
-		{
-			_putchar(a + '0');
-			if (a == 98)
-				continue;
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 	else
 	{
-		for (b = n; b <= 98; b++)
-		{
-			_putchar(b + '0');
-			if (b == 98)
-				continue;
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-	_putchar('\n');
 }
