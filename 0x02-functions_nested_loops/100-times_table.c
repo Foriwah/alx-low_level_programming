52393f6680ba884bb2ee3ccae75f5ddadb3926d4
Foriwah <akosuaforiwah00@gmail.com>
2023-04-25 14:36:22 -0700

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the nine times table
+ * @n: input variable
+ * Return: return 0
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int value;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= n; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			value = i * j;
+			if (value < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if ((value >= 10) && (value <= 99))
+				_putchar(' ');
+			else
+				_putchar((value / 10) + '0');
+
+			_putchar((value % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
