3dc53b11af86fb788398d5541a806d6f904448c3
Foriwah <akosuaforiwah00@gmail.com>
2023-04-25 06:09:39 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @d: input variable
+ * Return: Return a value
+ */
+
+int print_last_digit(int d)
+{
+	int d = d % 10;
+
+	_putchar(48 + d);
+}
