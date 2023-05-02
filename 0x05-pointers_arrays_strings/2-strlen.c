b3de8da4b0361d407f1bda729c214f21b6e44ee3
Foriwah <akosuaforiwah00@gmail.com>
2023-05-02 04:15:46 -0700

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - length of the string
+ * @s: character length
+ * Return: Return a value
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
