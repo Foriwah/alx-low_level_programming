7ba7cf33fd00145eadf0aed8609bc22d7bf7aa6b
Foriwah <akosuaforiwah00@gmail.com>
2023-05-04 14:17:22 -0700

    Capital letter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,11 +2,11 @@
 
 /**
  * cap_string - Capitalize all words
- * @n: pointer
+ * @str: pointer
  * Return: Return a value
  */
 
-char *cap_string(char *n)
+char *cap_string(char *str)
 {
 	int i = 0;
 
@@ -26,7 +26,7 @@ char *cap_string(char *n)
 			str[i - 1] == '(' ||
 			str[i - 1] == ')' ||
 			str[i - 1] == '{' ||
-			srr[i - 1] == '}' ||
+			str[i - 1] == '}' ||
 			i == 0)
 			str[i] -= 32;
 		i++;
