25e7c26a2042265a188392554d2ff0286576d78e
Foriwah <akosuaforiwah00@gmail.com>
2023-05-10 12:35:10 -0700

    Reverse a string

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - raised to the power
+ * @x: a varible
+ * @y: a variable
+ * Return: Return a value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	return (pow(double x, double y));
+}
