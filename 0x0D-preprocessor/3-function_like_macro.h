3e982af7183f2f9a6830c05b237993ba9c02d8f9
Foriwah <akosuaforiwah00@gmail.com>
2023-05-22 05:46:21 -0700

    Absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,11 @@
-#ifndef ABS
+#ifndef ABSOLUTE_H
+#define ABSOLUTE_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Desc: A header file for absolute value
+ * @x: the value
+ */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 #endif
