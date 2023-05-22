3a9acaef84db08891f7184ce50e1b072d89d1de1
Foriwah <akosuaforiwah00@gmail.com>
2023-05-22 05:59:02 -0700

    Sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,12 @@
-#ifndef SUM
-#define SUM(x,y) ((x) + (y))
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * @x: a variable
+ * @y: another variable
+ */
+
+#define SUM(x, y) ((x) + (y))
+
 #endif
