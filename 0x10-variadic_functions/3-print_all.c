9e246835f75617b889254b120160964d50433ea0
Foriwah <akosuaforiwah00@gmail.com>
2023-05-25 10:08:30 -0700

    Print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -63,3 +63,41 @@ void print_str(va_list list)
 	}
 	printf("%s", str);
 }
+
+/**
+ * print_all - print them all
+ * @format: the datatypes
+ * @...: our ellipses
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0, j = 0;
+	char *separator = "";
+	prtn_t func[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(list, format);
+
+	while (format && (*(format + i)))
+	{
+		j = 0;
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
+
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
