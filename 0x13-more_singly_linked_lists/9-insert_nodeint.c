6b225e3ee25d397e8d858ab1a4e3b260b7e82cae
Foriwah <akosuaforiwah00@gmail.com>
2023-06-06 06:23:51 -0700

    Insert at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node;
-       	listint_t *temp = NULL;
+	listint_t *temp = NULL;
 	unsigned int i = idx;
 
 	if (head == NULL)
