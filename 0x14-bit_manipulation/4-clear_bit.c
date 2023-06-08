a2d176f627d0250ce762766b978c7ea0bc0ab580
Foriwah <akosuaforiwah00@gmail.com>
2023-06-08 06:18:06 -0700

    clear bits

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - clear bit
+ * @n: the value
+ * @index: my index
+ * Return: return an int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = *n & ~(1ul << index);
+	return (1);
+}
