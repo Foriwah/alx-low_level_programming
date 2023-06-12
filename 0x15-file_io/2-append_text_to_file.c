5d0b8414b499dcacc956d31a7d158ed037fd1b54
Foriwah <akosuaforiwah00@gmail.com>
2023-06-12 05:30:34 -0700

    Append a text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,7 +29,6 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 		length = write(myfile, text_content, len);
 	}
-	if (close(myfile) == -1 || len != length)
-		return (-1);
-	return (length);
+	close(myfile);
+	return (length == len ? 1 : -1);
 }
