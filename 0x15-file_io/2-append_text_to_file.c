8084a906375f671afa6e40fa1ece78d1eef5fc5e
Foriwah <akosuaforiwah00@gmail.com>
2023-06-12 05:14:55 -0700

    Append a text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,10 +22,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	myfile = open(filename, O_WRONLY | O_APPEND);
 	if (myfile == -1)
 		return (-1);
-	len = 0;
-	while (text_content[len] != '\0')
-		len++;
-	length = write(myfile, text_content, len);
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+			len++;
+		length = write(myfile, text_content, len);
+	}
 	if (close(myfile) == -1 || len != length)
 		return (-1);
 	return (length);
