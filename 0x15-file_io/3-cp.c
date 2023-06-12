4fa04d79f5a8bbf50b01a40753647ed382cdefbd
Foriwah <akosuaforiwah00@gmail.com>
2023-06-12 06:58:12 -0700

    Cp from to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,23 +10,43 @@
 #define ERR_NW "Error: Can't write to %s\n"
 #define ERR_CL "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+/**
+ * _close - close file
+ * @fd: file descriptor
+ * Return: something
+ */
+
+int _close(int fd)
+{
+	int err;
+
+	err = close(fd);
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_CL, fd);
+		exit(100);
+	}
+	return (0);
+}
+
 /**
  * main - the main file
  * @ac: argument counter
  * @av: arguments
+ * Return: return something
  */
 
-int main (int ac, char **av)
+int main(int ac, char **av)
 {
-	int f_fd = 0;
-	int t_fd = 0;
+	int f_fd = 0, t_fd = 0, err = 0;
 	char buf[1024];
 	ssize_t b;
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, USAGE);
-		exit(98);
+		exit(97);
 	}
 	f_fd = open(av[1], O_RDONLY);
 	if (f_fd == -1)
@@ -42,7 +62,7 @@ int main (int ac, char **av)
 	}
 	while ((b = read(f_fd, buf, 1024)) > 0)
 	{
-		if(write(t_fd, buf, b) != b)
+		if (write(t_fd, buf, b) != b)
 		{
 			dprintf(STDERR_FILENO, ERR_NW, av[2]);
 			exit(99);
@@ -53,17 +73,9 @@ int main (int ac, char **av)
 		dprintf(STDERR_FILENO, ERR_NR, av[1]);
 		exit(98);
 	}
-	f_fd = close(f_fd);
-	t_fd = close(t_fd);
-	if (f_fd)
-	{
-		dprintf(STDERR_FILENO, ERR_CL, f_fd);
-		exit(100);
-	}
-	if (t_fd)
-	{
-		dprintf(STDERR_FILENO, ERR_CL, t_fd);
-		exit(100);
-	}
-	return (1);
+	err = _close(f_fd);
+	err += _close(t_fd);
+	if (err != 0)
+		return (100);
+	return (0);
 }
