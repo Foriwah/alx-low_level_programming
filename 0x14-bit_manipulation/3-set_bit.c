66191ab923d0affa7edfb6ffca13dbb246c0e62f
Foriwah <akosuaforiwah00@gmail.com>
2023-07-06 05:45:25 -0700

    Getting and setting bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_bit - set a bit
+ * @n: the value
+ * @index: the index to set
+ * Return: return an int
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = *n | 1ul << index;
+	return (1);
+}
